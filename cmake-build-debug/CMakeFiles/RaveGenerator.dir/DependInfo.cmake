91bf89e2c10c77c7dfff771b29ec463d30dce27d
Aefalzion <vasya.yugov@yandex.ru>
2017-10-17 13:06:15 +0300

    asdf

--- cmake-build-debug/CMakeFiles/RaveGenerator.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/RaveGenerator.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/vasya/Work/RaveGenerator/main.c" "C:/Users/vasya/Work/RaveGenerator/cmake-build-debug/CMakeFiles/RaveGenerator.dir/main.c.obj"
+  "C:/Users/vasya/Work/RaveProject/main.c" "C:/Users/vasya/Work/RaveProject/cmake-build-debug/CMakeFiles/RaveGenerator.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main() {
     printf("Hello, World!\n");
+    qwerty
     return 0;
 }
