814cfc22bb7fa20dad1b22e62b7255fb8da2586b
Aefalzion <vasya.yugov@yandex.ru>
2017-10-17 13:27:39 +0300

    Added introduction

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Git!\n");
+    printf("Hello! This is the Rave Generator. You can use following commands:\n"
+                   "i <filename>   to import file with text\n"
+                   "g <number>     to generate a text of <number> length\n");
+    printf(">");
     return 0;
 }
